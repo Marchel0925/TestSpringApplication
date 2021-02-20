7b397c9e3c6de5137b9512115f35ba8615de895b
Mārcis Andersons <marcis.andersons0925@gmail.com>
2021-02-20 10:19:48 +0200

    Exercise 2. and Exercise 3.

--- src/main/java/com/test/ex2/MainApplication.java
+++ src/main/java/com/test/ex2/MainApplication.java
@@ -1,17 +1,18 @@
 package com.test.ex2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MainApplication implements CommandLineRunner {
 
-    NamePrintingComponent namePrintingComponent;
-    PhoneNumberPrintingComponent phoneNumberPrintingComponent;
+    private final NamePrintingComponent namePrintingComponent;
+    @Autowired
+    private PhoneNumberPrintingComponent phoneNumberPrintingComponent;
 
-    public MainApplication(NamePrintingComponent namePrintingComponent, PhoneNumberPrintingComponent phoneNumberPrintingComponent) {
+    public MainApplication(NamePrintingComponent namePrintingComponent) {
         this.namePrintingComponent = namePrintingComponent;
-        this.phoneNumberPrintingComponent = phoneNumberPrintingComponent;
     }
 
 
